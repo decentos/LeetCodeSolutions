e65f0d21ba9ce9e29ee85aa512e66f89375e8e1f
Vlad Matveev <matveev_vlad@me.com>
2023-01-17 05:51:47 +0300

    solved 226, 487 and 1004

---
+++ src/me/decentos/leetcode/medium/M1004MaxConsecutiveOnes3.java
@@ -0,0 +1,35 @@
+package me.decentos.leetcode.medium;
+
+public class M1004MaxConsecutiveOnes3 {
+
+    public int longestOnes(int[] nums, int k) {
+        int subCount = 0, zeroCount = 0;
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] == 0) zeroCount++;
+
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            subCount = Math.max(subCount, right - left + 1);
+            right++;
+        }
+        return subCount;
+    }
+
+    public int longestOnes2(int[] nums, int k) {
+        int left = 0, right;
+        for (right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) k--;
+
+            if (k < 0) {
+                k += 1 - nums[left];
+                left++;
+            }
+        }
+        return right - left;
+    }
+}

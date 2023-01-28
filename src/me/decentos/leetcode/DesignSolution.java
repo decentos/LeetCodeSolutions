bf4d38b36eb2ef3a50603950103edd8d545c9013
Vlad Matveev <matveev_vlad@me.com>
2023-01-28 05:29:19 +0300

    solved 257, 703

--- src/me/decentos/leetcode/DesignSolution.java
+++ src/me/decentos/leetcode/DesignSolution.java
@@ -1,5 +1,7 @@
 package me.decentos.leetcode;
 
+import me.decentos.leetcode.design.E703KthLargestInStream;
+
 public class DesignSolution {
     public static void main(String[] args) {
 //        M707DesignLinkedList head = new M707DesignLinkedList();
@@ -21,5 +23,11 @@ public class DesignSolution {
 //        head.addAtIndex(5,0);
 //        head.addAtHead(6);
 //        DoublyNode head = DoublyNode.createMock();
+        E703KthLargestInStream e703 = new E703KthLargestInStream(3, new int[]{4, 5, 8, 2});
+        e703.add(3);  // return 4
+        e703.add(5);  // return 5
+        e703.add(10); // return 5
+        e703.add(9);  // return 8
+        e703.add(4);  // return 8
     }
 }

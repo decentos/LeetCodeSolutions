59b4574f5f1713246990a16f90015e62d629fbbc
Vlad Matveev <matveev_vlad@me.com>
2023-01-31 05:38:56 +0300

    solved 973, 1167, 1642

---
+++ src/me/decentos/leetcode/medium/M1167MinimumCost.java
@@ -0,0 +1,22 @@
+package me.decentos.leetcode.medium;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class M1167MinimumCost {
+
+    public int connectSticks(int[] sticks) {
+        if (sticks.length < 2) return 0;
+        int sum = 0;
+        Queue<Integer> heap = new PriorityQueue<>();
+        for (int s : sticks) heap.offer(s);
+
+        while (heap.size() > 1) {
+            int s1 = heap.poll(), s2 = heap.poll();
+            int currSum = s1 + s2;
+            sum += currSum;
+            heap.offer(currSum);
+        }
+        return sum;
+    }
+}

ce50c86f4de38a95fe6c9870660632fc74631ede
Vlad Matveev <matveev_vlad@me.com>
2023-02-01 05:44:51 +0300

    solved 295, 705, 706

--- src/me/decentos/leetcode/design/E170TwoSum3.java
+++ src/me/decentos/leetcode/design/E170TwoSum3.java
@@ -5,34 +5,31 @@ import java.util.Collections;
 import java.util.List;
 
 public class E170TwoSum3 {
+    private final List<Integer> numbers;
+    private boolean isSorted;
 
-    class TwoSum {
-        private final List<Integer> numbers;
-        private boolean isSorted;
+    public E170TwoSum3() {
+        numbers = new ArrayList<>();
+        isSorted = false;
+    }
 
-        public TwoSum() {
-            numbers = new ArrayList<>();
-            isSorted = false;
+    public void add(int number) {
+        numbers.add(number);
+        isSorted = false;
+    }
+
+    public boolean find(int value) {
+        if (!isSorted) {
+            Collections.sort(numbers);
+            isSorted = true;
         }
-
-        public void add(int number) {
-            numbers.add(number);
-            isSorted = false;
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers.get(l) + numbers.get(r);
+            if (sum == value) return true;
+            else if (sum > value) r--;
+            else l++;
         }
-
-        public boolean find(int value) {
-            if (!isSorted) {
-                Collections.sort(numbers);
-                isSorted = true;
-            }
-            int l = 0, r = numbers.size() - 1;
-            while (l < r) {
-                int sum = numbers.get(l) + numbers.get(r);
-                if (sum == value) return true;
-                else if (sum > value) r--;
-                else l++;
-            }
-            return false;
-        }
+        return false;
     }
 }

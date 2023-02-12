ddaece8ff6101811074e6db25f5638149b1e91b1
Vlad Matveev <matveev_vlad@me.com>
2023-02-12 06:18:36 +0300

    solved 190, 191

---
+++ leetcode/easy/E190ReverseBits.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class E190ReverseBits {
+
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
---
+++ leetcode/easy/E191Number1Bits.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class E191Number1Bits {
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        String binary = Integer.toBinaryString(n);
+        for (char i : binary.toCharArray()) {
+            if (i == '1') count++;
+        }
+        return count;
+    }
+
+    public int hammingWeight2(int n) {
+        return Integer.bitCount(n);
+    }
+}

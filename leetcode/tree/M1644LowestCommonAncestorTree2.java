da0306e48b99856061f5fefe0d41882dbe1b6f8a
Vlad Matveev <matveev_vlad@me.com>
2023-02-20 07:44:39 +0300

    solved 1644

--- leetcode/tree/M1644LowestCommonAncestorTree2.java
+++ leetcode/tree/M1644LowestCommonAncestorTree2.java
@@ -2,18 +2,27 @@ package tree;
 
 import util.TreeNode;
 
-public class M236LowestCommonAncestorTree {
+public class M1644LowestCommonAncestorTree2 {
     private TreeNode answer;
+    private boolean hasP, hasQ;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         findAncestor(root, p, q);
-        return answer;
+        if (hasP && hasQ) return answer;
+        return null;
     }
 
     private boolean findAncestor(TreeNode node, TreeNode p, TreeNode q) {
         if (node == null) return false;
 
-        int curr = (node == p || node == q) ? 1 : 0;
+        int curr = 0;
+        if (node == p) {
+            curr++;
+            hasP = true;
+        } else if (node == q) {
+            curr++;
+            hasQ = true;
+        }
         int left = findAncestor(node.left, p, q) ? 1 : 0;
         int right = findAncestor(node.right, p, q) ? 1 : 0;
         int sum = curr + left + right;
@@ -24,10 +33,22 @@ public class M236LowestCommonAncestorTree {
 
 // ===============================================================================
 
+    private int count;
+
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor2(root.left, p, q);
-        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+        TreeNode ancestor = findAncestor2(root, p, q);
+        return count == 2 ? ancestor : null;
+    }
+
+    private TreeNode findAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        TreeNode left = findAncestor2(root.left, p, q);
+        TreeNode right = findAncestor2(root.right, p, q);
+
+        if (root == p || root == q) {
+            count++;
+            return root;
+        }
         if (left == null) return right;
         if (right == null) return left;
         return root;

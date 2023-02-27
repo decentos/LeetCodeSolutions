68fd5f0295bcce2fcc74d9e43b7b9bbf4d441aaa
Vlad Matveev <matveev_vlad@me.com>
2023-02-27 06:11:48 +0300

    solved 173, 510, 1586

--- leetcode/medium/M394DecodeString.java
+++ leetcode/medium/M394DecodeString.java
@@ -1,5 +1,9 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class M394DecodeString {
     private int index = 0;
 
@@ -31,5 +35,42 @@ public class M394DecodeString {
             }
         }
         return decode.toString();
+    }
+
+// ===============================================================================
+
+    public String decodeString2(String s) {
+        Map<Integer, Integer> brackets = new HashMap<>();
+        Stack<Integer> openBrackets = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '[') {
+                openBrackets.push(i);
+            } else if (curr == ']') {
+                int openBracket = openBrackets.pop();
+                brackets.put(openBracket, i);
+            }
+        }
+        return decode(s, brackets, 0, s.length() - 1);
+    }
+
+    private String decode(String s, Map<Integer, Integer> brackets, int l, int r) {
+        StringBuilder sb = new StringBuilder();
+        int times = 0;
+        for (int i = l; i <= r; i++) {
+            char curr = s.charAt(i);
+            if (Character.isLetter(curr)) {
+                sb.append(curr);
+            } else if (Character.isDigit(curr)) {
+                times = times * 10 + curr - '0';
+            } else if (curr == '[') {
+                int closeBracket = brackets.get(i);
+                String nested = decode(s, brackets, i + 1, closeBracket - 1);
+                sb.append(nested.repeat(times));
+                times = 0;
+                i = closeBracket;
+            }
+        }
+        return sb.toString();
     }
 }

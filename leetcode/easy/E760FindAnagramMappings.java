1ee0490a0b5a41773dfdc9a92b502fe10f8af8ff
Vlad Matveev <matveev_vlad@me.com>
2023-04-26 18:26:55 +0300

    solved 760

---
+++ leetcode/easy/E760FindAnagramMappings.java
@@ -0,0 +1,22 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class E760FindAnagramMappings {
+
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int[] ans = new int[n];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(nums2[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+}

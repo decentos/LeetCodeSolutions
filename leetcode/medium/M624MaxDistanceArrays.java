7bab606b5cc93e815888cccdec8a011fbdb65e79
Vlad Matveev <matveev_vlad@me.com>
2023-05-27 13:14:28 +0300

    solved 624

---
+++ leetcode/medium/M624MaxDistanceArrays.java
@@ -0,0 +1,22 @@
+package medium;
+
+import java.util.List;
+
+public class M624MaxDistanceArrays {
+
+    public int maxDistance(List<List<Integer>> arrays) {
+        List<Integer> first = arrays.get(0);
+        int min = first.get(0);
+        int max = first.get(first.size() - 1);
+        int ans = 0;
+
+        for (int i = 1; i < arrays.size(); i++) {
+            List<Integer> curr = arrays.get(i);
+            ans = Math.max(ans, max - curr.get(0));
+            ans = Math.max(ans, curr.get(curr.size() - 1) - min);
+            min = Math.min(min, curr.get(0));
+            max = Math.max(max, curr.get(curr.size() - 1));
+        }
+        return ans;
+    }
+}

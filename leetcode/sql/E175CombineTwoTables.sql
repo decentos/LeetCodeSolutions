3fbb9bd6ce4d9516bbbf371e86070f64a2258a05
Vlad Matveev <matveev_vlad@me.com>
2023-09-11 07:57:52 +0200

    solved 1378

--- leetcode/sql/E175CombineTwoTables.sql
+++ leetcode/sql/E175CombineTwoTables.sql
@@ -1,4 +1,4 @@
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
-         LEFT JOIN Address a
-                   ON p.personId = a.personId;
+LEFT JOIN Address a
+ON p.personId = a.personId;
--- leetcode/sql/E512GamePlayAnalysis2.sql
+++ leetcode/sql/E512GamePlayAnalysis2.sql
@@ -3,4 +3,5 @@ FROM Activity
 WHERE (player_id, event_date) IN (
     SELECT player_id, MIN(event_date)
     FROM Activity
-    GROUP BY player_id);
+    GROUP BY player_id
+);

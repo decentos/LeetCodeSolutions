23d0e9e78979df78d4dd90c0ec6877d22da7d194
Vlad Matveev <matveev_vlad@me.com>
2023-12-15 08:22:17 +0100

    solved 1436

---
+++ leetcode/easy/E1436DestinationCity.java
@@ -0,0 +1,23 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class E1436DestinationCity {
+
+    public String destCity(List<List<String>> paths) {
+        Set<String> from = new HashSet<>();
+
+        for (List<String> path : paths) {
+            from.add(path.get(0));
+        }
+
+        for (List<String> path : paths) {
+            if (!from.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+        return "";
+    }
+}

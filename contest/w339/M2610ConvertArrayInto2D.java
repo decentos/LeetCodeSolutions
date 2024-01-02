4dddebf73649943a35f0180c60a293c6d27d9cf0
Vlad Matveev <matveev_vlad@me.com>
2024-01-02 08:00:08 +0100

    solved 2610

--- contest/w339/M2610ConvertArrayInto2D.java
+++ contest/w339/M2610ConvertArrayInto2D.java
@@ -1,24 +1,46 @@
 package w339;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class M2610ConvertArrayInto2D {
 
     public List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) map.merge(num, 1, Integer::sum);
-        while (true) {
+        int[] freq = new int[nums.length + 1];
+
+        for (int num : nums) {
+            if (freq[num] >= ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+            ans.get(freq[num]).add(num);
+            freq[num]++;
+        }
+        return ans;
+    }
+
+// ===============================================================================
+
+    public List<List<Integer>> findMatrix2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<Integer> row = new HashSet<>();
+        int n = nums.length, count = n;
+
+        while (count > 0) {
             List<Integer> curr = new ArrayList<>();
-            int index = ans.size() + 1;
-            map.forEach((k,v) -> {
-                if (v >= index) curr.add(k);
-            });
-            if (curr.isEmpty()) break;
+            for (int i = 0; i < n; i++) {
+                int num = nums[i];
+                if (num > 0 && !row.contains(num)) {
+                    curr.add(num);
+                    row.add(num);
+                    count--;
+                    nums[i] = 0;
+                }
+            }
             ans.add(curr);
+            row.clear();
         }
         return ans;
     }

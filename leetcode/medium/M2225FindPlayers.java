405bc05ad45e1e2ef14306fa8151e0cafbdd9ad7
Vlad Matveev <matveev_vlad@me.com>
2024-01-15 08:17:05 +0100

    solved 2225

---
+++ leetcode/medium/M2225FindPlayers.java
@@ -0,0 +1,66 @@
+package medium;
+
+import java.util.*;
+
+public class M2225FindPlayers {
+
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<Integer> winners = new HashSet<>();
+        Set<Integer> losers = new HashSet<>();
+        Set<Integer> oneLosses = new HashSet<>();
+
+        for (int[] match : matches) {
+            if (!losers.contains(match[1])) {
+                losers.add(match[1]);
+                oneLosses.add(match[1]);
+            } else {
+                oneLosses.remove(match[1]);
+            }
+
+            winners.remove(match[1]);
+            if (!losers.contains(match[0])) {
+                winners.add(match[0]);
+            }
+        }
+
+        ans.add(winners.stream().sorted().toList());
+        ans.add(oneLosses.stream().sorted().toList());
+        return ans;
+    }
+
+// ===============================================================================
+
+    public List<List<Integer>> findWinners2(int[][] matches) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> zeroLoss = new ArrayList<>();
+        List<Integer> oneLoss = new ArrayList<>();
+        ans.add(zeroLoss);
+        ans.add(oneLoss);
+        int[] counter = new int[100_001];
+        Arrays.fill(counter, -1);
+
+        for (int[] match : matches) {
+            int winner = match[0], loser = match[1];
+
+            if (counter[winner] == -1) {
+                counter[winner] = 0;
+            }
+
+            if (counter[loser] == -1) {
+                counter[loser] = 1;
+            } else {
+                counter[loser]++;
+            }
+        }
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] == 0) {
+                zeroLoss.add(i);
+            } else if (counter[i] == 1) {
+                oneLoss.add(i);
+            }
+        }
+        return ans;
+    }
+}
--- leetcode/string/M1657CloseStrings.java
+++ leetcode/string/M1657CloseStrings.java
@@ -1,26 +1,37 @@
 package string;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class M1657CloseStrings {
 
-    public static boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) return false;
+    public boolean closeStrings(String word1, String word2) {
+        int[] counter1 = calculateLetters(word1);
+        int[] counter2 = calculateLetters(word2);
+        for (int i = 0; i < counter1.length; i++) {
+            if (counter1[i] > 0 && counter2[i] == 0 || counter2[i] > 0 && counter1[i] == 0) {
+                return false;
+            }
+        }
 
-        Map<Character, Integer> map1 = new HashMap<>();
-        for (char c : word1.toCharArray()) map1.merge(c, 1, Integer::sum);
-        Map<Character, Integer> map2 = new HashMap<>();
-        for (char c : word2.toCharArray()) map2.merge(c, 1, Integer::sum);
-        if (!map1.keySet().equals(map2.keySet())) return false;
+        Map<Integer, Integer> occurs1 = calculateOccurs(counter1);
+        Map<Integer, Integer> occurs2 = calculateOccurs(counter2);
+        return occurs1.equals(occurs2);
+    }
 
-        List<Integer> list1 = new ArrayList<>(map1.values());
-        List<Integer> list2 = new ArrayList<>(map2.values());
-        Collections.sort(list1);
-        Collections.sort(list2);
-        return list1.equals(list2);
+    private int[] calculateLetters(String word) {
+        int[] counter = new int[26];
+        for (char c : word.toCharArray()) {
+            counter[c - 'a']++;
+        }
+        return counter;
+    }
+
+    private Map<Integer, Integer> calculateOccurs(int[] counter) {
+        Map<Integer, Integer> occurs = new HashMap<>();
+        for (int i : counter) {
+            occurs.merge(i, 1, Integer::sum);
+        }
+        return occurs;
     }
 }

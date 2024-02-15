86cc49717eae67e51048eea787c97ca73ce2c6a3
Vlad Matveev <matveev_vlad@me.com>
2024-02-15 09:14:52 +0100

    solved 2971

---
+++ leetcode/medium/M2971FindPolygonPerimeter.java
@@ -0,0 +1,19 @@
+package medium;
+
+import java.util.Arrays;
+
+public class M2971FindPolygonPerimeter {
+
+    public long largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        long sum = 0, perimeter = -1;
+
+        for (int num : nums) {
+            if (sum > num) {
+                perimeter = sum + num;
+            }
+            sum += num;
+        }
+        return perimeter;
+    }
+}

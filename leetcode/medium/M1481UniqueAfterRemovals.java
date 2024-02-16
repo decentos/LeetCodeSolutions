fd5b6c375a6c12233c9f4cd5c7795ecddf5577fc
Vlad Matveev <matveev_vlad@me.com>
2024-02-16 07:06:23 +0100

    solved 1481

---
+++ leetcode/medium/M1481UniqueAfterRemovals.java
@@ -0,0 +1,36 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class M1481UniqueAfterRemovals {
+
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> unique = new HashMap<>();
+        int[] count = new int[arr.length + 1];
+
+        for (int i : arr) {
+            unique.merge(i, 1, Integer::sum);
+        }
+
+        for (int i : unique.values()) {
+            count[i]++;
+        }
+
+        int ans = unique.size();
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0) {
+                continue;
+            }
+
+            if (k - i * count[i] >= 0) {
+                ans -= count[i];
+                k -= i * count[i];
+            } else {
+                ans -= k / i;
+                return ans;
+            }
+        }
+        return 0;
+    }
+}

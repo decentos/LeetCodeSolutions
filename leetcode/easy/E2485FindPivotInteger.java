2eeb8d38332582e87214cc735c1facc27e92958c
Vlad Matveev <matveev_vlad@me.com>
2024-03-13 07:16:36 +0100

    solved 2485

---
+++ leetcode/easy/E2485FindPivotInteger.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class E2485FindPivotInteger {
+
+    public int pivotInteger(int n) {
+        int leftSum = n * (n + 1) / 2, rightSum = 0;
+
+        for (int i = n; i > 0; i--) {
+            rightSum += i;
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum -= i;
+        }
+        return -1;
+    }
+
+    public int pivotInteger2(int n) {
+        int sum = n * (n + 1) / 2;
+        int pivot = (int) Math.sqrt(sum);
+        return pivot * pivot == sum ? pivot : -1;
+    }
+}

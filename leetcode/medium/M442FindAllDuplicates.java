4fd7ce4e637267b351621f7a657d20654bb5a9f5
Vlad Matveev <matveev_vlad@me.com>
2024-03-31 14:03:47 +0200

    solved 897

--- leetcode/medium/M442FindAllDuplicates.java
+++ leetcode/medium/M442FindAllDuplicates.java
@@ -1,52 +1,21 @@
 package medium;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class M442FindAllDuplicates {
 
     public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
         List<Integer> dups = new ArrayList<>();
-        Set<Integer> unique = new HashSet<>();
 
-        for (int num : nums) {
-            if (unique.contains(num)) {
-                dups.add(num);
+        for (int i = 0; i < n; i++) {
+            int position = Math.abs(nums[i]) - 1;
+            if (nums[position] < 0) {
+                dups.add(position + 1);
             } else {
-                unique.add(num);
+                nums[position] *= -1;
             }
-        }
-        return dups;
-    }
-
-    public List<Integer> findDuplicates2(int[] nums) {
-        List<Integer> dups = new ArrayList<>();
-
-        for (int num : nums) {
-            nums[Math.abs(num) - 1] *= -1;
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            int curr = Math.abs(nums[i]);
-            if (nums[curr - 1] > 0) {
-                dups.add(curr);
-                nums[curr - 1] *= -1;
-            }
-        }
-        return dups;
-    }
-
-    public List<Integer> findDuplicates3(int[] nums) {
-        List<Integer> dups = new ArrayList<>();
-
-        for (int num : nums) {
-            int abs = Math.abs(num);
-            if (nums[abs - 1] < 0) {
-                dups.add(abs);
-            }
-            nums[abs - 1] *= -1;
         }
         return dups;
     }

b83c0105d12ac4705fbf9298544f3406dd5ad550
Vlad Matveev <matveev_vlad@me.com>
2024-04-10 07:43:28 +0200

    solved 950

---
+++ leetcode/medium/M950RevealCardsIncreasing.java
@@ -0,0 +1,31 @@
+package medium;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class M950RevealCardsIncreasing {
+
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n = deck.length;
+        int[] order = new int[n];
+        int orderIndex = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int top = queue.poll();
+            order[top] = deck[orderIndex++];
+
+            if (!queue.isEmpty()) {
+                int bottom = queue.poll();
+                queue.offer(bottom);
+            }
+        }
+        return order;
+    }
+}

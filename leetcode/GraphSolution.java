b0df2255ceda858ba46614a58342b6a49c50eed7
Vlad Matveev <matveev_vlad@me.com>
2024-04-10 10:14:31 +0200

    solved 1202

---
+++ leetcode/GraphSolution.java
@@ -0,0 +1,10 @@
+import graph.unionfind.M1202SmallestStringSwaps;
+
+import java.util.List;
+
+public class GraphSolution {
+    public static void main(String[] args) {
+        M1202SmallestStringSwaps m1202 = new M1202SmallestStringSwaps();
+        m1202.smallestStringWithSwaps("dcab", List.of(List.of(0, 3), List.of(1, 2)));
+    }
+}
---
+++ leetcode/graph/unionfind/M1202SmallestStringSwaps.java
@@ -0,0 +1,75 @@
+package graph.unionfind;
+
+import java.util.*;
+
+public class M1202SmallestStringSwaps {
+
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        int n = s.length();
+        char[] result = new char[n];
+        UnionFind uf = new UnionFind(n);
+
+        for (List<Integer> pair : pairs) {
+            uf.union(pair.get(0), pair.get(1));
+        }
+
+        Map<Integer, List<Integer>> roots = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int root = uf.find(i);
+            roots.computeIfAbsent(root, val -> new ArrayList<>()).add(i);
+        }
+
+        roots.values().forEach(indices -> {
+            List<Character> letters = new ArrayList<>();
+            for (int index : indices) {
+                letters.add(s.charAt(index));
+            }
+
+            Collections.sort(letters);
+
+            for (int i = 0; i < letters.size(); i++) {
+                result[indices.get(i)] = letters.get(i);
+            }
+        });
+
+        return String.valueOf(result);
+    }
+
+    private static class UnionFind {
+        private final int[] root;
+        private final int[] rank;
+
+        public UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int node) {
+            if (root[node] == node) {
+                return node;
+            }
+            return root[node] = find(root[node]);
+        }
+
+        public void union(int nodeX, int nodeY) {
+            int rootX = find(nodeX);
+            int rootY = find(nodeY);
+
+            if (rootX != rootY) {
+                if (rank[rootX] < rank[rootY]) {
+                    root[rootY] = rootX;
+                } else if (rank[rootX] > rank[rootY]) {
+                    root[rootX] = rootY;
+                } else {
+                    root[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+    }
+}

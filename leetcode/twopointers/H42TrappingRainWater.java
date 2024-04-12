61c4a19b4e4ff02b0aa5c7c364d0e9e64a2a9942
Vlad Matveev <matveev_vlad@me.com>
2024-04-12 08:00:50 +0200

    solved 42

---
+++ leetcode/twopointers/H42TrappingRainWater.java
@@ -0,0 +1,28 @@
+package twopointers;
+
+public class H42TrappingRainWater {
+
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] > leftMax) {
+                leftMax = height[left];
+            }
+            if (height[right] > rightMax) {
+                rightMax = height[right];
+            }
+
+            if (leftMax < rightMax) {
+                water += leftMax - height[left];
+                left++;
+            } else {
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+}

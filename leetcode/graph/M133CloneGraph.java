49e01028df7e93c2c7b24770e789fd6974213cb5
Vlad Matveev <matveev_vlad@me.com>
2024-04-14 09:56:44 +0200

    solved 133

--- leetcode/graph/M133CloneGraph.java
+++ leetcode/graph/M133CloneGraph.java
@@ -8,18 +8,23 @@ import java.util.Map;
 public class M133CloneGraph {
 
     public NTreeNode cloneGraph(NTreeNode node) {
+        if (node == null) {
+            return null;
+        }
         return copy(node, new HashMap<>());
     }
 
-    private NTreeNode copy(NTreeNode node, Map<NTreeNode, NTreeNode> map) {
-        if (node == null) return null;
-        if (map.containsKey(node)) return map.get(node);
+    private NTreeNode copy(NTreeNode node, Map<Integer, NTreeNode> visited) {
+        if (visited.containsKey(node.val)) {
+            return visited.get(node.val);
+        }
 
         NTreeNode copy = new NTreeNode(node.val);
-        map.put(node, copy);
+        visited.put(node.val, copy);
 
-        for (NTreeNode child : node.children) {
-            copy.children.add(copy(child, map));
+        for (NTreeNode neighbor : node.children) {
+            NTreeNode copyNeighbor = copy(neighbor, visited);
+            copy.children.add(copyNeighbor);
         }
         return copy;
     }

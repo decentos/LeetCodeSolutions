22a79d4a7ee4a5e5dca62c3b0a6ecc6b635d5000
Vlad Matveev <matveev_vlad@me.com>
2024-04-17 07:58:29 +0200

    solved 988, 1469

---
+++ leetcode/tree/E1469FindLonelyNodes.java
@@ -0,0 +1,26 @@
+package tree;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class E1469FindLonelyNodes {
+
+    public List<Integer> getLonelyNodes(TreeNode root) {
+        List<Integer> lonely = new ArrayList<>();
+        dfs(root, false, lonely);
+        return lonely;
+    }
+
+    private void dfs(TreeNode curr, boolean isLonely, List<Integer> lonely) {
+        if (curr == null) {
+            return;
+        }
+        if (isLonely) {
+            lonely.add(curr.val);
+        }
+        dfs(curr.left, curr.right == null, lonely);
+        dfs(curr.right, curr.left == null, lonely);
+    }
+}

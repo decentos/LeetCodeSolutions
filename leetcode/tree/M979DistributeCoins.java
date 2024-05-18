c6f4a00c629d878ac2445b83b50832155376871c
Vlad Matveev <matveev_vlad@me.com>
2024-05-18 07:30:21 +0200

    solved 979

---
+++ leetcode/tree/M979DistributeCoins.java
@@ -0,0 +1,25 @@
+package tree;
+
+import util.TreeNode;
+
+public class M979DistributeCoins {
+    private int moves;
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return moves;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        moves += Math.abs(left) + Math.abs(right);
+
+        return root.val - 1 + left + right;
+    }
+}

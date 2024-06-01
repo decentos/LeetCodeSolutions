7ea02a0c66164a75b222c80978b5c5268398c4b8
Vlad Matveev <matveev_vlad@me.com>
2024-06-01 08:18:34 +0200

    solved 3130

---
+++ leetcode/string/E3110ScoreString.java
@@ -0,0 +1,12 @@
+package string;
+
+public class E3110ScoreString {
+
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return score;
+    }
+}

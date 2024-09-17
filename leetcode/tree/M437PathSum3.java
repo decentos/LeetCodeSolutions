7b215f2e9625e9fd58b5f00cc54e3d0b3e58954a
Vlad Matveev <matveev_vlad@me.com>
2024-09-17 07:31:58 +0200

    solved 1372

--- leetcode/tree/M437PathSum3.java
+++ leetcode/tree/M437PathSum3.java
@@ -8,44 +8,28 @@ import java.util.Map;
 public class M437PathSum3 {
 
     public int pathSum(TreeNode root, int targetSum) {
-        if (root == null) return 0;
-        return dfs(root, targetSum, 0)
-                + pathSum(root.left, targetSum)
-                + pathSum(root.right, targetSum);
+        if (root == null) {
+            return 0;
+        }
+        return dfs(root, targetSum, 0, new HashMap<>());
     }
 
-    private int dfs(TreeNode node, int targetSum, long currSum) {
-        if (node == null) return 0;
+    private int dfs(TreeNode node, int targetSum, long currSum, Map<Long, Integer> sum) {
+        if (node == null) {
+            return 0;
+        }
 
-        int count = 0;
         currSum += node.val;
-        if (currSum == targetSum) count++;
 
-        count += dfs(node.left, targetSum, currSum);
-        count += dfs(node.right, targetSum, currSum);
+        int count = sum.getOrDefault(currSum - targetSum, 0);
+        if (currSum == targetSum) {
+            count++;
+        }
 
-        return count;
-    }
-
-// ===============================================================================
-
-    public int pathSum2(TreeNode root, int targetSum) {
-        return dfs(new HashMap<>(), root, targetSum, 0);
-    }
-
-    private int dfs(Map<Long, Integer> map, TreeNode node, int targetSum, long currSum) {
-        if (node == null) return 0;
-
-        int count = 0;
-        currSum += node.val;
-        if (targetSum == currSum) count++;
-        count += map.getOrDefault(currSum - targetSum, 0);
-
-        int freq = map.merge(currSum, 1, Integer::sum);
-        count += dfs(map, node.left, targetSum, currSum);
-        count += dfs(map, node.right, targetSum, currSum);
-        map.replace(currSum, freq - 1);
-
+        sum.merge(currSum, 1, Integer::sum);
+        count += dfs(node.left, targetSum, currSum, sum);
+        count += dfs(node.right, targetSum, currSum, sum);
+        sum.merge(currSum, -1, Integer::sum);
         return count;
     }
 }

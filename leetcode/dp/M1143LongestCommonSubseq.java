f85837ca6f6b8065ac649e5dc44b3f44a73e8cd0
Vlad Matveev <matveev_vlad@me.com>
2024-10-08 08:08:32 +0200

    solved 72

--- leetcode/dp/M1143LongestCommonSubseq.java
+++ leetcode/dp/M1143LongestCommonSubseq.java
@@ -3,18 +3,18 @@ package dp;
 public class M1143LongestCommonSubseq {
 
     public int longestCommonSubsequence(String text1, String text2) {
-        int n1 = text1.length(), n2 = text2.length();
-        int[][] dp = new int[n1 + 1][n2 + 1];
+        int len1 = text1.length(), len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
 
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n2; j++) {
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i + 1][j + 1] = dp[i][j] + 1;
+        for (int row = 1; row <= len1; row++) {
+            for (int col = 1; col <= len2; col++) {
+                if (text1.charAt(row - 1) == text2.charAt(col - 1)) {
+                    dp[row][col] = dp[row - 1][col - 1] + 1;
                 } else {
-                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                    dp[row][col] = Math.max(dp[row - 1][col], dp[row][col - 1]);
                 }
             }
         }
-        return dp[n1][n2];
+        return dp[len1][len2];
     }
 }

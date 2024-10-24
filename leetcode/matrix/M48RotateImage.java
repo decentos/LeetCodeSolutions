1a8f9bd5b132d556ab086052e32b27c374820d38
Vlad Matveev <matveev_vlad@me.com>
2024-10-24 15:55:21 +0200

    solved 1438

--- leetcode/matrix/M48RotateImage.java
+++ leetcode/matrix/M48RotateImage.java
@@ -3,13 +3,42 @@ package matrix;
 public class M48RotateImage {
 
     public void rotate(int[][] matrix) {
-        int n = matrix.length, m = matrix[0].length, start = 0;
-        while (start < m / 2) {
-            for (int i = start; i < m - 1 - start; i++) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+
+    private void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int row = 0; row < n; row++) {
+            for (int col = row + 1; col < n; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }
+    }
+
+    private void reverse(int[][] matrix) {
+        int n = matrix.length;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n / 2; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[row][n - col - 1];
+                matrix[row][n - col - 1] = temp;
+            }
+        }
+    }
+
+// ===============================================================================
+
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length, start = 0;
+        while (start < n / 2) {
+            for (int i = start; i < n - 1 - start; i++) {
                 int temp = matrix[n - 1 - i][start];
-                matrix[n - 1 - i][start] = matrix[n - 1 - start][m - 1 - i];
-                matrix[n - 1 - start][m - 1 - i] = matrix[i][m - 1 - start];
-                matrix[i][m - 1 - start] = matrix[start][i];
+                matrix[n - 1 - i][start] = matrix[n - 1 - start][n - 1 - i];
+                matrix[n - 1 - start][n - 1 - i] = matrix[i][n - 1 - start];
+                matrix[i][n - 1 - start] = matrix[start][i];
                 matrix[start][i] = temp;
             }
             start++;

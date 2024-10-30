dacae52f86c5a66d81ec1564b499db9d9413012b
Vlad Matveev <matveev_vlad@me.com>
2024-10-30 08:43:28 +0100

    solved 792

---
+++ companies/uber/M792MatchingSubsequences.java
@@ -0,0 +1,31 @@
+package uber;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class M792MatchingSubsequences {
+
+    public int numMatchingSubseq(String s, String[] words) {
+        int n = s.length(), count = 0;
+        Map<String, Integer> uniqueWords = new HashMap<>();
+
+        for (String word : words) {
+            uniqueWords.merge(word, 1, Integer::sum);
+        }
+
+        for (String word : uniqueWords.keySet()) {
+            int wordIndex = 0, m = word.length(), sIndex = 0;
+
+            while (wordIndex < m && sIndex < n) {
+                if (word.charAt(wordIndex) == s.charAt(sIndex)) {
+                    wordIndex++;
+                }
+                sIndex++;
+            }
+            if (wordIndex == m) {
+                count += uniqueWords.get(word);
+            }
+        }
+        return count;
+    }
+}

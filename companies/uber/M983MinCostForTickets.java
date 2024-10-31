f7994505939533f3dfba40a5cfd6fad9bf2ba536
Vlad Matveev <matveev_vlad@me.com>
2024-10-31 07:58:55 +0100

    solved 983

---
+++ companies/uber/M983MinCostForTickets.java
@@ -0,0 +1,23 @@
+package uber;
+
+public class M983MinCostForTickets {
+
+    public int mincostTickets(int[] days, int[] costs) {
+        int lastDay = days[days.length - 1], cost = 0;
+        int[] dp = new int[lastDay + 1];
+        int i = 0;
+
+        for (int day = 1; day <= lastDay; day++) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                i++;
+                int dayPassCost = dp[day - 1] + costs[0];
+                int weekPassCost = dp[Math.max(0, day - 7)] + costs[1];
+                int monthPassCost = dp[Math.max(0, day - 30)] + costs[2];
+                dp[day] = Math.min(dayPassCost, Math.min(weekPassCost, monthPassCost));
+            }
+        }
+        return dp[lastDay];
+    }
+}

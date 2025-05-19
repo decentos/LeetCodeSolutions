f41a5bd6be0cda8b172a53bd7f93c754948458ac
Vlad Matveev <matveev_vlad@me.com>
2025-05-19 08:29:51 +0200

    solved 239

---
+++ companies/booking/H239SlidingWindowMax.java
@@ -0,0 +1,34 @@
+package booking;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class H239SlidingWindowMax {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> max = new ArrayList<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < k; i++) {
+            while (!queue.isEmpty() && nums[i] >= nums[queue.peekLast()]) {
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+        }
+        max.add(nums[queue.peek()]);
+
+        for (int i = k; i < nums.length; i++) {
+            if (queue.peek() == i - k) {
+                queue.poll();
+            }
+            while (!queue.isEmpty() && (nums[i] >= nums[queue.peekLast()])) {
+                queue.pollLast();
+            }
+            queue.offerLast(i);
+            max.add(nums[queue.peek()]);
+        }
+        return max.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

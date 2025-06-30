501ef2fde6f6caeb684a026f2936b0f7f190e199
Vlad Matveev <matveev_vlad@me.com>
2025-06-30 08:12:33 +0200

    solved 2101, 2672

---
+++ leetcode/graph/M2101DetonateMaxBombs.java
@@ -0,0 +1,54 @@
+package graph;
+
+import java.util.*;
+
+public class M2101DetonateMaxBombs {
+
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
+        int maxCount = 0;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (inRange(bombs[i], bombs[j])) {
+                    adj.computeIfAbsent(i, val -> new ArrayList<>()).add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int currCount = 0;
+            boolean[] visited = new boolean[n];
+            Deque<Integer> queue = new ArrayDeque<>();
+            queue.offer(i);
+            visited[i] = true;
+
+            while (!queue.isEmpty()) {
+                int curr = queue.poll();
+                currCount++;
+
+                if (adj.containsKey(curr)) {
+                    List<Integer> neighbors = adj.get(curr);
+                    for (int neighbor : neighbors) {
+                        if (!visited[neighbor]) {
+                            visited[neighbor] = true;
+                            queue.offer(neighbor);
+                        }
+                    }
+                }
+            }
+            maxCount = Math.max(maxCount, currCount);
+        }
+        return maxCount;
+    }
+
+    private boolean inRange(int[] a, int[] b) {
+        long dx = a[0] - b[0], dy = a[1] - b[1], r = a[2];
+        return dx * dx + dy * dy <= r * r;
+    }
+}
---
+++ leetcode/medium/M2672NumberAdjColor.java
@@ -0,0 +1,27 @@
+package medium;
+
+public class M2672NumberAdjColor {
+
+    public int[] colorTheArray(int n, int[][] queries) {
+        int m = queries.length;
+        int[] sameColor = new int[m];
+        int[] colors = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            int queryIndex = queries[i][0];
+            int queryColor = queries[i][1];
+            int count = i > 0 ? sameColor[i - 1] : 0;
+
+            if (colors[queryIndex] != 0) {
+                count += queryIndex == 0 ? 0 : colors[queryIndex - 1] == colors[queryIndex] ? -1 : 0;
+                count += queryIndex == n - 1 ? 0 : colors[queryIndex + 1] == colors[queryIndex] ? -1 : 0;
+            }
+
+            colors[queryIndex] = queryColor;
+            count += queryIndex == 0 ? 0 : colors[queryIndex - 1] == colors[queryIndex] ? 1 : 0;
+            count += queryIndex == n - 1 ? 0 : colors[queryIndex + 1] == colors[queryIndex] ? 1 : 0;
+            sameColor[i] = count;
+        }
+        return sameColor;
+    }
+}
